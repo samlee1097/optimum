a237cb82c42bb6091dc476d247a429454236a5db
Samuel <samlee1097@yahoo.com>
2021-12-01 02:54:08 -0500

    logs

--- client/src/Components/ProfilePage/LogCard.js
+++ client/src/Components/ProfilePage/LogCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import "../../Styling/LogCard.css"
 
 function LogCard({log}) {
-    console.log(log)
     return (
        <div className="log-container">
-           
+           <div class='card'>
+                <div class='info'>
+                    <h1 class='title'>{log.activity_type}</h1>
+                    <p class='description'>
+                        <p>Duration: {log.activity_duration} minutes</p>
+                        <p>Date: {log.date}</p>
+                    </p>
+                </div>
+            </div>
        </div>
     );
 }
