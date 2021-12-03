04592e494efe4f5a637f72800567465dae7302df
Samuel <samlee1097@yahoo.com>
2021-12-02 19:00:33 -0500

    working on cleaning up logs

--- client/src/Components/ProfilePage/LogCard.js
+++ client/src/Components/ProfilePage/LogCard.js
@@ -2,17 +2,23 @@ import React from 'react';
 import "../../Styling/LogCard.css"
 
 function LogCard({log}) {
+    console.log(log)
     return (
        <div className="log-container">
            <div class='card'>
                 <div class='info'>
-                    <h1 class='title'>{log.activity_type}</h1>
-                    <p class='description'>
-                        <span className="log-details">
-                        <p>Duration: {log.activity_duration} minutes</p>
-                        <p>Date: {log.date}</p>
+                    <span className="log-details">
+                        <h1 class='title'>{log.activity_type}</h1>
+                        <span className="log-details">  
+                            <p>Duration: {log.activity_duration} minutes</p>
+                            <p>Date: {log.date}</p>
                         </span>
-                    </p>
+                        <div className="extra-details">
+                            <p>Happiness: {log.happiness}/5</p>
+                            <p>Weight: {log.weight}lbs</p>
+                            {log.notes !=="" ? <p>Note: {log.notes}</p> : null}
+                        </div>
+                    </span>
                 </div>
             </div>
        </div>
